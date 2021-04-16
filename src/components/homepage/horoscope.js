377473f82512869349c4865f676c49e9bb1985c6
shashank <shashanksingh42@gmail.com>
2021-04-16 22:17:17 +0530

    incomplete horoscope section with overflow image

---
+++ src/components/homepage/horoscope.js
@@ -0,0 +1,172 @@
+import React, { Component, Fragment } from "react";
+import horoscope from "./Capricorn.png"
+
+export default class Horoscope extends Component {
+  render() {
+    return (
+      <Fragment>
+        <section className='horoscope-section'>
+          <div className="container">
+            <h2 className='horoscope__title'>Free Daily Horoscopes</h2>
+            <div className="row horoscope-sign-container">
+              <div className="col-12 col-lg-3 test">
+                <div className="row">
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                          <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                      <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                      <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+             
+                </div>
+              </div>
+              <div className="col-12 col-lg-3 test">
+                <div className="row">
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                          <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                      <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                      <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+             
+                </div>
+              </div>
+              <div className="col-12 col-lg-3 test">
+                <div className="row">
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                          <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                      <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                      <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+             
+                </div>
+              </div>
+              <div className="col-12 col-lg-3 test">
+                <div className="row">
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                          <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                      <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-4 test-2">
+                      <div className='horoscope-item'>
+                      <div className='horoscope-item__img-container'>
+                          <img className='horoscope-item__img' src={horoscope} alt=""/>
+                          </div>
+                          <div className='horoscope-item__container'>
+                              <h3 className='horoscope-item__title'>aries</h3>
+                              <div className='horoscope-item__date'>23 oct -24 nov</div>
+                          </div>
+                      </div>
+                  </div>
+             
+                </div>
+              </div>
+          
+     
+            </div>
+          </div>
+        </section>
+      </Fragment>
+    );
+  }
+}
--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -4,11 +4,13 @@ import HowItWorks from './howItWorks'
 import FeaturedAstrologers from './featuredAstrologers'
 import Testimonials from "./testimonials.js"
 import Footer from "./footer"
+import Horoscope from './horoscope.js'
 export default class index extends Component {
   render() {
     return (
       <section>
         <Jumbotron showModal={this.props.showModal}></Jumbotron>
+        <Horoscope></Horoscope>
         <HowItWorks></HowItWorks>
         <FeaturedAstrologers></FeaturedAstrologers>
         <Testimonials></Testimonials>
